68f139b78f370fa343acc6196f940e49fd4cc3a7
hirano.shigetoshi <hirano.shigetoshi@classmethod.jp>
2024-09-10 08:32:13 +0900

    長さを色で表現

--- lua/VimJKMotion.lua
+++ lua/VimJKMotion.lua
@@ -4,6 +4,9 @@ local ns_id = vim.api.nvim_create_namespace("right_line_numbers")
 local alphas = { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "q", "r", "s", "t", "u", "v", "w",
     "x", "y", "z", ";a", ";b", ";c", ";d", ";e", ";f", ";g", ";h", ";i", ";j", ";k", ";l", ";m", ";n", ";q", ";r", ";s",
     ";t", ";u", ";v", ";w", ";x", ";y", ";z" }
+vim.api.nvim_command('highlight PrimaryColor guifg=darkgray')
+vim.api.nvim_command('highlight SecondaryColor guifg=gray')
+local color_schemas = { "PrimaryColor", "SecondaryColor" }
 
 local function findIndex(arr, element)
     for i, value in ipairs(arr) do
@@ -19,6 +22,10 @@ local function getMark(c)
         return string.sub(c, -1)
     end
     return c
+end
+
+local function getSchema(c)
+    return color_schemas[string.len(c)]
 end
 
 M.motion = function()
@@ -42,8 +49,9 @@ local function set_right_line_numbers(bufnr)
 
     for i = first_line, last_line do
         local visible_line_num = i - first_line + 1
+        local alpha = alphas[visible_line_num]
         vim.api.nvim_buf_set_extmark(bufnr, ns_id, i, 0, {
-            virt_text = { { getMark(alphas[visible_line_num]), "MoreMsg" } },
+            virt_text = { { getMark(alpha), getSchema(alpha) } },
             virt_text_win_col = -999,
             virt_text_pos = "overlay",
         })
